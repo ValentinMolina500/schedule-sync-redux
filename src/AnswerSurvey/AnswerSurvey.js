87b8cb28aeb5ecec0aa58dd9de1a2d323288b577
Valentin Molina <valentin.molina@wsu.edu>
2020-07-30 01:02:05 -0700

    prototype answer poll page

--- src/AnswerSurvey/AnswerSurvey.js
+++ src/AnswerSurvey/AnswerSurvey.js
@@ -1,33 +1,48 @@
 import React, { useState } from 'react';
 import "./AnswerSurvey.css";
 import logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
 
 export default () => {
     const [buttonActive, setButtonActive] = useState(false)
     return (
         <div className="AnswerSurvey">
+            
             <div className="main-page-container">
+            <nav className="header">
+                <img className="header-logo" src={logo} />
+                <ul>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/new-survey"}>Create Poll</Link></li>
+                </ul>
+            </nav>
                 <div className="main-content-container">
                     
                     <div className="info-container">
-                        <img className="logo" src={logo} />
+                        <div className="logo-container">
+                            <img className="logo" src={logo} />
+
+                        </div>
                         <div className="action-bar-container">
                            
-                            <h1 className="headline">Answer Survey</h1>
+                            <h1 className="headline">When are you free?</h1>
                         </div>
                         <div className="title-date-container"> 
                             <span className="event-title">Ford's Tesla Unveiling</span>
                             <span className="middle-dot">&middot;</span>
                             <span className="date-text">Jun 19, 2020</span>
                         </div> 
+                        <div className="description-container">
+                            Ford finally got his Tesla so we are meeting up at his place to check it out. By the way this is very long.
+                        </div>
                     </div>
                     
                     <div className="time-selection-container">
-                        <div style={{gridColumn: "1"}}>10:30AM</div>
-                        <div style={{gridColumn: "2"}}>11:00AM</div>
+                        <div>10:30AM</div>
+                        <div>11:00AM</div>
                         <div onClick={() => setButtonActive(!buttonActive)} className={`${buttonActive ? 'active' : ''}`}>
                             <div className="inner-button">
-                                <div>
+                                <div className="button-head">
                                     11:30AM
                                 </div>
                                 <div className={`button-sub ${buttonActive ? "" : 'hidden'}`}> 
